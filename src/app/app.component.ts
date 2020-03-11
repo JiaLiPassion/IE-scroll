83507aa74834b06b5a315a2db93b763097cb168f
JiaLiPassion <JiaLi.Passion@gmail.com>
2020-03-11 18:42:25 +0900

    try to reproduce scroll issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'scroll';
+export class AppComponent implements AfterViewInit {
+  name = 'Angular';
+
+  ngAfterViewInit() {
+    window.addEventListener('scroll', e => console.log('scroll'))
+  }
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,15 +18,31 @@
  * BROWSER POLYFILLS
  */
 
+import 'core-js';
+// import 'core-js/es6/object';
+// import 'core-js/es6/function';
+// import 'core-js/es6/parse-int';
+// import 'core-js/es6/parse-float';
+// import 'core-js/es6/number';
+// import 'core-js/es6/math';
+// import 'core-js/es6/string';
+// import 'core-js/es6/date';
+// import 'core-js/es6/regexp';
+// import 'core-js/es6/map';
+// import 'core-js/es6/weak-map';
+// import 'core-js/es6/set';
+// import 'core-js/es6/array';
+// import 'core-js/es7/array'; // for .includes()
+
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
  */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
@@ -55,9 +71,11 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
+import './zone-flag';
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+// import 'core-js/es7/array';
